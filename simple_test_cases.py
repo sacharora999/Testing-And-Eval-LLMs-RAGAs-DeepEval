d3a878081ab1e4e809d20ef4dd5900960339901d
Sachin Arora <132284536+sacharora999@users.noreply.github.com>
2026-02-23 12:32:35 +0530

    Update simple_test_cases.py

--- simple_test_cases.py
+++ simple_test_cases.py
@@ -10,3 +10,17 @@ test_case = LLMTestCase(
 
 answer_relevancy_met.measure(test_case)
 print(answer_relevancy_met.score)
+
+
+from deepeval.test_case import LLMTestCase
+from deepeval.metrics import ContextualPrecisionMetric
+
+cpm =  ContextualPrecisionMetric()
+test_case = LLMTestCase(
+    input="Who is the current prsident of USA ?",
+    expected_output="Donald Trump",
+    retrieval_context=["Donald Trump is the current president of the USA"]
+)
+
+cpm.measure(test_case)
+print(cpm.score)
